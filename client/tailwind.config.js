6899cef8b4573857c38a815853a2d18d73255401
Yashash <yashash1111@users.noreply.github.com>
2026-05-29 04:19:34 +0530

    Center containers globally

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -6,6 +6,13 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
     extend: {
       colors: {
         border: "hsl(var(--border))",
